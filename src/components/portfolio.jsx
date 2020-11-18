0fd650af12078110505a3b3c52f8c9a1efbf5c61
catalin <catalin.casuneanu@gmail.com>
2020-11-18 14:47:31 +0200

    sonarlint

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -4,9 +4,7 @@ import React from "react";
 import stock from "../img/image1.jpg";
 import stock1 from "../img/image2.jpg";
 import stock2 from "../img/image3.jpg";
-import stock3 from "../img/image4.jpg";
-import stock4 from "../img/image5.jpg";
-import stock5 from "../img/image6.jpg";
+
 
 class Portfolio extends React.Component {
   render() {
