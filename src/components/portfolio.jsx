4e96db6041da2bfc4ecca1d83d6a003e6f0b788f
CatalinCsnMaster <catalin.casuneanu@gmail.com>
2021-02-18 19:40:00 +0200

    reduce

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -57,31 +57,7 @@ export default function Portofolios() {
                 
               </div>
             </div>
-            <div className="col-md-4" style={{height:'100%'}}>
-              <div className="work-box">
-                <a href="https://www.mrcleaner.de/" target="blank_" rel="noopener">
-                  <div className="work-img">
-                    <img src={stock2} alt="mr cleaner" className="img-fluid" />
-                  </div></a>
-                  <div className="work-content">
-                    <div className="row">
-                      <div className="col-sm-8">
-                        <h2 className="w-title">Mr Clean</h2>
-                        <div className="w-more">
-                          <span className="w-ctegory">
-                            Php, Laravel
-                          </span>
-                          {/*} / <span className="w-date">18 Sep. 2018</span>*/}
-                        </div>
-                      </div>
-                      <div className="col-sm-4">
-                        <div className="w-like">
-                        </div>
-                      </div>
-                    </div>
-                  </div>          
-              </div>
-            </div>
+           
             <div className="col-md-4">
               <div className="work-box">
                 <a href="https://www.nabservices.at/" target="blank_" rel="noopener">
