7b38cfcb2ddc7a31f7126485a155257a9f755fc4
Casuneanu Catalin <catalin.casuneanu@gmail.com>
2021-12-02 00:22:23 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from '../components/navbar.jsx';
 import Intro from '../components/intro.js';
-import Footer from '../components/footer.js';
 
 export default function Home(){
     return (
         <div>
         <Navbar/>
         <Intro/>
-        <Footer/>
         </div>
     );
 }
