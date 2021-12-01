e05c8bdba0572a7b1c9dd42e315a1d714190f3c1
Casuneanu Catalin <catalin.casuneanu@gmail.com>
2021-12-02 00:44:36 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,13 +14,15 @@ function Footer(){
                 <Col style={{textAlign:'left'}}><a href="https://www.facebook.com/jaysoftwarero" target="blank" style={{textDecoration:'none'}}>
                     <FontAwesomeIcon icon={faFacebook} size="2x" color="#007BFF"/></a> &nbsp; &nbsp;
 
-                        <a href="https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Flaurentiu_noss%3Figshid%3D1pjqqzjmmnkkw%26fbclid%3DIwAR0Xr_nPOuuyaQ_1mbw72FvsfDst924L7buJEYAdot2Sys8fENtU-q5V_9w&h=AT2Jdf_oSq8fqjILyBCXl8IxZnbwAdXPdYUoiIicYw_PFKSoQkpJIciIo5VPnKyD8XKA9-PLqEsDPBPH2O8CFaGBNzW53Eu0GARwXwELRi4lqLLfdmnWSJHPvxd337uTNB-GDnTClQ4" target="blank" style={{textDecoration:'none'}}>
+                        <a href="https://www.instagram.com/jay_software/" target="blank" style={{textDecoration:'none'}}>
                     <FontAwesomeIcon icon={faInstagram} size="2x" color="#007BFF"/> </a>&nbsp; &nbsp;   </Col>
             </Row>
+            <Row>
+                 <Col style={{textAlign:'left'}}>
+                     <p>Jay Software {t("powered")} <a href="https://jobster-synergy.com" target="_blank" rel="noreferrer">Jobster-Synergy Group</a></p> 
+                 </Col>
+            </Row>
         </Container>
-        <div style={{textAlign:'left', marginTop:'5%'}}>
-        <p>&nbsp; &nbsp;Jay Software {t("powered")} <a href="https://jobster-synergy.com" target="_blank" rel="noreferrer">Jobster-Synergy Group</a></p> 
-        </div></div>
     );
 }
 
