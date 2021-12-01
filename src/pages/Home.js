99053fecfc982a1569c2408a925c33e0eeb4cea5
Casuneanu Catalin <catalin.casuneanu@gmail.com>
2021-12-02 00:05:39 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,9 @@ import React from 'react';
 import Navbar from '../components/navbar.jsx';
 import Intro from '../components/intro.js';
 import Footer from '../components/footer.js';
-import First from '../img/webdev.svg';
-import {Row, Col, Container} from 'react-bootstrap'
-import InfiniteCarousel from 'react-leaf-carousel';
-import {Link} from 'react-router-dom'
-import {motion} from 'framer-motion'
-import {useTranslation} from "react-i18next";
+import {Container} from 'react-bootstrap'
 
 export default function Home(){
-   const [t] = useTranslation();
     return (
         <div>
         <Navbar/>
